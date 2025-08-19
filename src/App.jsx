237672ee1b39566d7338dc8e5edf3b698e9cb23a
biyash-1 <sthaalish39@gmail.com>
2025-08-19 21:57:11 +0545

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Statecolor from "./Components/Statecolor";
-import Interview from "./Components/Interview";
-import Navbar from "./Components/Navbar";
-import Button from "./Components/Button";
-import { counterContext } from "./context/context";
-import Todo from "./Components/Todo";
-import Datafetching from "./Components/Datafetching";
-import Quiz from "./Components/Quiz";
-import OwnTodo from "./Components/OwnTodo";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import ProfileList from "./Components/ProfileList";
-import Question1 from "./Components/Question1"
-import Tabs from "./Components/Tabs";
-import Todo from "./Components/Todo";
+import Otp from "./Components/Otp";
+
 const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<ProfileList />}></Route>
-            <Route path = "/state" element = {<Question1 />}></Route>
-            <Route path = "/tab" element = {<Tabs/>} />
-            <Route path = "/todo" element = {<Todo/>}/>
-          </Routes>
-        </Router>
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/otp" element={<Otp />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
